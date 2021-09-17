755f7e22dabc2e811adee44333edfd4689f79c85
Jonas Vihoalé ANIGLO <46029829+JIV-DLS@users.noreply.github.com>
2021-09-17 23:38:48 +0200

    get database url from env debug

--- Server/app.py
+++ Server/app.py
@@ -4,10 +4,11 @@ from flask import jsonify
 import psycopg2
 import os
 
+database = os.environ.get('DATABASE_URL', 'database')
+
 print(os.environ['DATABASE_URL'] if 'DATABASE_URL' in os.environ['DATABASE_URL'] else 'database')
 conn = psycopg2.connect(
-    host=os.environ['DATABASE_URL'] if 'DATABASE_URL' in os.environ['DATABASE_URL'] else 'database',
-    port=5432,
+    host=database,
     database="td_1",
     user="si5_sacc",
     password="dev_password")
